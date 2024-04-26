8f77d8fcbb9849f31d2bb8bd9b7bb0751a6b7a89
Jae Liu <34577583+jayeliu@users.noreply.github.com>
2024-04-27 01:16:33 +0800

    Update clouddrive.rb

--- Formula/clouddrive.rb
+++ Formula/clouddrive.rb
@@ -5,7 +5,7 @@ class Clouddrive < Formula
   desc "CloudDrive is a powerful multi-cloud disk management tool that provides users with a one-stop multi-cloud disk solution that includes cloud disk local mount."
   homepage "https://www.clouddrive2.com/index.html"
   url "https://github.com/jayeliu/homebrew-taps/blob/main/pkg/clouddrive-0.6.13.tar.gz"
-  sha256 "ecb92fb09818d2350cdc883c7744a05cb7309ea55e55bd6fba99211c50f14fb5"
+  sha256 "9e2c8ca719c4c6269ed38e015ef60c048c2b5353ae3e622e990510dbd70e0add"
   license "None"
 
   # depends_on "cmake" => :build
